605199fa1fd99c05712a07e0c508eadbd2438ec6
Brian Kinsella <>
2023-03-18 23:10:27 +0000

    trying something

--- src/test/java/com/solenersync/userstore/UserstoreProviderTests.java
+++ src/test/java/com/solenersync/userstore/UserstoreProviderTests.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -24,6 +25,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @IgnoreNoPactsToVerify
 @IgnoreMissingStateChange
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("pact-provider")
 public class UserstoreProviderTests {
 
     @MockBean
